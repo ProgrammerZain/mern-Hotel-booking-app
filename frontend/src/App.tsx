16716e375dbeff9e68d6e8b7e1478e0f225ec7f7
zain irfan <zainirfan565@gmail.com>
2024-05-10 23:58:35 +0500

    starting project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,29 @@
+import { Router, Routes, Route } from "react-router-dom";
+import Layout from "./layouts/Layout";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Layout>
+              <p>Home Page</p>
+            </Layout>
+          }
+        />
+        <Route
+          path="/search"
+          element={
+            <Layout>
+              <p>Search Page</p>
+            </Layout>
+          }
+        />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
