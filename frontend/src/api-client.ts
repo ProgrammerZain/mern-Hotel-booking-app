b8755040ae9796460d8623bfba422aafdeb24aa1
zain irfan <zainirfan565@gmail.com>
2024-05-12 20:21:25 +0500

    Add: signin and signout

--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -1,7 +1,8 @@
 import { RegisterFormData } from "./pages/Register";
+import { SignInFormData } from "./pages/SignIn";
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 export const register = async (formdata: RegisterFormData) => {
-  const response = await fetch(`http://localhost:7000/api/users/register`, {
+  const response = await fetch(`${API_BASE_URL}/api/users/register`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -11,5 +12,41 @@ export const register = async (formdata: RegisterFormData) => {
   const responseBody = await response.json();
   if (!response.ok) {
     throw new Error(responseBody.message);
+  }
+};
+export const signIn = async (FormData: SignInFormData) => {
+  const response = await fetch(`${API_BASE_URL}/api/auth/login`, {
+    method: "POST",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(FormData),
+  });
+
+  const body = await response.json();
+  if (!response.ok) {
+    throw new Error(body.message);
+  }
+  return body;
+};
+
+export const validateToken = async function () {
+  const response = await fetch(`${API_BASE_URL}/api/auth/validate-token`, {
+    credentials: "include",
+  });
+  if (!response.ok) {
+    throw new Error("Token Invalid");
+  }
+  return response.json();
+};
+
+export const signOut = async () => {
+  const response = await fetch(`${API_BASE_URL}/api/auth/logout`, {
+    credentials: "include",
+    method: "POST",
+  });
+  if (!response.ok) {
+    throw new Error("Error during sign out");
   }
 };
